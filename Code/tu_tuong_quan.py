afa119d74fa93e219a50d6150fc0d45411d2c27b
DuyVo2005 <vlkd1001@gmail.com>
2025-12-15 21:29:36 +0700

    up code tu_tuong_quan, my_lpc

---
+++ Code/tu_tuong_quan.py
@@ -0,0 +1,26 @@
+import numpy as np
+import librosa
+
+x = np.arange(1, 201)
+# y = librosa.autocorrelate(x)
+y = librosa.autocorrelate(x, max_size=12) # 0 -> 11
+N = len(x)
+print(y)
+R0 = 0
+k = 0
+for n in range(0, N):
+    R0 = R0 + x[n]*x[n+k]
+print(R0)
+
+R11 = 0
+k = 11
+for n in range(0, N):
+    chi_so_sau = n+k
+    if chi_so_sau > N - 1:
+        temp = 0
+    else:
+        temp = x[n+k]
+    R11 = R11 + x[n]*temp
+print(R11)
+
+# https://librosa.org/doc/main/generated/librosa.autocorrelate.html
